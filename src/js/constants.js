0c3fc084837bb85785aacd0b99720eadb64e6b32
kumartul <akumarofficial4224@gmail.com>
2022-01-21 16:48:45 +0530

    [Feature]: Declare some constants

---
+++ src/js/constants.js
@@ -0,0 +1,5 @@
+export const confirmationStates = ["Yes", "No"];
+export const statements = [
+    "Do you want your Regex to start with a particular character or character set?",
+    "Do you want your Regex to end with a particular character or character set?"
+];
