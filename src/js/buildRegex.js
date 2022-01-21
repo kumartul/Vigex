26c7bf062c1beb46301a4625bbc95e5a58286d62
kumartul <akumarofficial4224@gmail.com>
2022-01-21 19:48:43 +0530

    [Feature]: Generate regexes with 'starts-with' parameters

--- src/js/buildRegex.js
+++ src/js/buildRegex.js
@@ -1,5 +1,7 @@
 const generateRegexBtn = document.getElementById('generate-regex');
 
+const formElements = document.querySelectorAll('.form-element');
+
 // Function: Generates the regular expression
 generateRegexBtn.addEventListener('click', () => {    
     generateRegexBtn.textContent = "Generating...";
@@ -10,4 +12,57 @@ generateRegexBtn.addEventListener('click', () => {
     generateRegexBtn.style.backgroundColor = "#1b9bff";
     generateRegexBtn.style.color = "white";
     generateRegexBtn.style.cursor = "not-allowed";
+
+    let regex = ``;
+
+    formElements.forEach(formElement => {
+        if(formElement.querySelector('select').selectedIndex === 0) {
+            if(formElement.id === "starts-with") {
+                regex = `^`;
+
+                const radioBtns = [...formElement.querySelectorAll('input[type="radio"]')];
+    
+                radioBtns.forEach(radioBtn => {
+                    if(radioBtn.checked) {
+                        if(radioBtn.className === "character-set") {
+                            const checkBoxes = [...formElement.querySelectorAll('.character-sets input')];
+
+                            checkBoxes.forEach(checkBox => {
+                                if(checkBox.checked) {
+                                    if(checkBox.id.includes("uppercase")) {
+                                        const lastPortion = regex.slice(regex.indexOf("^") + 1);
+                                        regex = regex.slice(0, regex.indexOf("^") + 1) + "A-Z" + lastPortion;
+                                    }
+                                    if(checkBox.id.includes("lowercase")) {
+                                        const lastPortion = regex.slice(regex.indexOf("^") + 1);
+                                        regex = regex.slice(0, regex.indexOf("^") + 1) + "a-z" + lastPortion;
+                                    }
+                                    if(checkBox.id.includes("numbers")) {
+                                        const lastPortion = regex.slice(regex.indexOf("^") + 1);
+                                        regex = regex.slice(0, regex.indexOf("^") + 1) + "0-9" + lastPortion;
+                                    }
+                                    if(checkBox.id.includes("special")) {
+                                        const lastPortion = regex.slice(regex.indexOf("^") + 1);
+                                        regex = regex.slice(0, regex.indexOf("^") + 1) + "_\\W" + lastPortion;
+                                    }
+
+                                }
+                            });
+                            
+                            const lastPortion = regex.slice(regex.indexOf("^") + 1);
+                            regex = regex.slice(0, regex.indexOf("^") + 1) + "[" + lastPortion + "]";
+                        }
+                        else if(radioBtn.className === "custom-characters") {
+                            const customCharactersField = formElement.querySelector('.custom-characters-field input');
+                            
+                            regex = `^[${customCharactersField.value}]`;
+                        }
+                    }
+                });
+            }
+        }
+    });
+
+    regex = "/" + regex + "/";
+    console.log(regex);
 });
