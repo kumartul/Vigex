bd5eff9fe8b10bb9262297a95f9304ebbc0e38db
kumartul <akumarofficial4224@gmail.com>
2022-01-22 19:36:18 +0530

    [Refactor]: Add useful comments

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,8 +1,11 @@
 import { regexKeyName } from './utils.js';
 
+// Function: Saves the regex in localStorage in the form of an object
 export const saveToLocalStorage = (regexName, regex) => {
+    // Fetch the item from the localStorage
     let regexArr = localStorage.getItem(regexKeyName);
 
+    // If the regex is not null and not empty, then only parse it else initialize it with an empty array
     if(regexArr !== null && regex !== []) {
         regexArr = JSON.parse(regexArr);
     }
