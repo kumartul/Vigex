c6064cb57a6c865533f8a9284c54972213301366
kumartul <akumarofficial4224@gmail.com>
2022-01-22 19:32:02 +0530

    [Refactor]: Add comments

--- src/js/alert.js
+++ src/js/alert.js
@@ -3,14 +3,18 @@ export default alertBox;
 
 const hideAlertBtn = alertBox.querySelector('button');
 
+// Attach a 'click' event listener to the hideAlertBtn so that whenever someone clicks on that button, the
+// alert box disappears
 hideAlertBtn.addEventListener('click', () => {
     hideAlertBox(alertBox);
 });
 
+// Function: Hides the alert box
 export const hideAlertBox = alertBox => {
     alertBox.style.display = "none"; 
 }
 
+// Function: Displays the alert box with a message in it
 export const showAlertBox = (alertBox, message) => {
     alertBox.style.display = "flex";
 
