4e0344eb2bfad86377898930cc72955c71fa4db2
--global <kumartul001@gmail.com>
2022-02-15 09:51:45 +0530

    [Lint]: utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,30 +1,22 @@
-export const confirmationStates = ["Yes", "No"];
+export const confirmationStates = ['Yes', 'No'];
 export const statements = [
-    "Do you want your Regex to start with a particular character or character set?",
-    "Do you want your Regex to end with a particular character or character set?"
+	'Do you want your Regex to start with a particular character or character set?',
+	'Do you want your Regex to end with a particular character or character set?'
 ];
 export const formElementIDs = [
-    "starts-with",
-    "ends-with"
+	'starts-with',
+	'ends-with'
 ];
 
 export const flags = [
-    {
-        flag: "g",
-        text: "Global Search"
-    },
-    {
-        flag: "i",
-        text: "Case Insensitive Search"
-    },
-    {
-        flag: "m",
-        text: "Multi-line Mode"
-    },
-    {
-        flag: "u",
-        text: "Support Unicode Characters"
-    }
-]
+	{ 'flag': 'g',
+		'text': 'Global Search' },
+	{ 'flag': 'i',
+		'text': 'Case Insensitive Search' },
+	{ 'flag': 'm',
+		'text': 'Multi-line Mode' },
+	{ 'flag': 'u',
+		'text': 'Support Unicode Characters' }
+];
 
-export const regexKeyName = "regex";
+export const regexKeyName = 'regex';
